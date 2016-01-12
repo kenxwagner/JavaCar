db435f8f85b6a6dadba16d6baa556f30a3079be3
Ken Wagner <kenwagner@hotmail.com>
2016-01-12 14:22:43 -0800

    Added more objects

--- src/javacar/JavaCar.java
+++ src/javacar/JavaCar.java
@@ -7,139 +7,161 @@ package javacar;
  */
 public class JavaCar {
 
+     String nameOfCar = "Average Car";
     int maxSpeedMph = 150;
     int maxSpeedKph = 200;
     double zeroToSixty = 9.5; //zeroto60times.com and estimated for sedan
-    int maxHP = 300;
+    int maxHP = 200;
     double startingPrice = 33560; //Kelly Blue book
-    String nameOfCar = "Average Car";
+   
 	
     
     public void printVariables() {
         System.out.println("Car Model: " + nameOfCar);
         System.out.println("It has a maximum speed of: " + maxSpeedMph + " Mph");
         System.out.println("Maximum speed in Metric: " + maxSpeedKph + " Kph");
-        System.out.println("Zero to Sixty: " + zeroToSixty + " Mph");
+        System.out.println("Zero to Sixty: " + zeroToSixty + " Seconds");
         System.out.println("horsepower: " + maxHP);
         System.out.println("You have to spend at least: $" + startingPrice);
         
     } 
     
-    public void fastCar1() { 
-String nameOfCar = "Hennessey Venom GT"; 
-int maxSpeedMph = 270; 
-int maxSpeedKph = 435; 
-double zeroToSixty = 2.5; 
-int maxHP = 1244; 
-double startingPrice = 1000000; 
+public void fastCar1() { 
+nameOfCar = "Hennessey Venom GT"; 
+maxSpeedMph = 270; 
+maxSpeedKph = 435; 
+zeroToSixty = 2.5; 
+maxHP = 1244; 
+startingPrice = 1000000; 
 }
 public void fastCar2() { 
-String nameOfCar = "Bugatti Veyron Super Sport"; 
-int maxSpeedMph = 268; 
-int maxSpeedKph = 431; 
-double zeroToSixty = 2.4; 
-int maxHP = 1200; 
-double startingPrice = 2400000; 
+ nameOfCar = "Bugatti Veyron Super Sport"; 
+ maxSpeedMph = 268; 
+ maxSpeedKph = 431; 
+ zeroToSixty = 2.4; 
+ maxHP = 1200; 
+ startingPrice = 2400000; 
 }
 public void fastCar3() { 
-String nameOfCar = "Koenigsegg Agera R"; 
-int maxSpeedMph = 260; 
-int maxSpeedKph = 418; 
-double zeroToSixty = 2.9; 
-int maxHP = 1099; 
-double startingPrice = 1600000; 
+ nameOfCar = "Koenigsegg Agera R"; 
+ maxSpeedMph = 260; 
+ maxSpeedKph = 418; 
+ zeroToSixty = 2.9; 
+ maxHP = 1099; 
+ startingPrice = 1600000; 
 }
 public void fastCar4() { 
-String nameOfCar = "SSC Ultimate Aero"; 
-int maxSpeedMph = 257; 
-int maxSpeedKph = 413; 
-double zeroToSixty = 2.7; 
-int maxHP = 1183; 
-double startingPrice = 654400; 
+ nameOfCar = "SSC Ultimate Aero"; 
+ maxSpeedMph = 257; 
+ maxSpeedKph = 413; 
+ zeroToSixty = 2.7; 
+ maxHP = 1183; 
+ startingPrice = 654400; 
 }
 public void fastCar5() { 
-String nameOfCar = "9ff GT9-R"; 
-int maxSpeedMph = 257; 
-int maxSpeedKph = 413; 
-double zeroToSixty = 2.9; 
-int maxHP = 1120; 
-double startingPrice = 695000; 
+ nameOfCar = "9ff GT9-R"; 
+ maxSpeedMph = 257; 
+ maxSpeedKph = 413; 
+ zeroToSixty = 2.9; 
+ maxHP = 1120; 
+ startingPrice = 695000; 
 }
 public void fastCar6() { 
-String nameOfCar = " Saleen S7 Twin-Turbo"; 
-int maxSpeedMph = 248; 
-int maxSpeedKph = 399; 
-double zeroToSixty = 2.8; 
-int maxHP = 750; 
-double startingPrice = 555000; 
+ nameOfCar = " Saleen S7 Twin-Turbo"; 
+ maxSpeedMph = 248; 
+ maxSpeedKph = 399; 
+ zeroToSixty = 2.8; 
+ maxHP = 750; 
+ startingPrice = 555000; 
 }
 public void fastCar7() { 
-String nameOfCar = " Koenigsegg CCX"; 
-int maxSpeedMph = 245; 
-int maxSpeedKph = 394; 
-double zeroToSixty = 3.2; 
-int maxHP = 806; 
-double startingPrice = 545568; 
+ nameOfCar = " Koenigsegg CCX"; 
+ maxSpeedMph = 245; 
+ maxSpeedKph = 394; 
+ zeroToSixty = 3.2; 
+ maxHP = 806; 
+ startingPrice = 545568; 
 }
 public void fastCar8() { 
-String nameOfCar = " McLaren F1"; 
-int maxSpeedMph = 240; 
-int maxSpeedKph = 386; 
-double zeroToSixty = 3.2; 
-int maxHP = 627; 
-double startingPrice = 970000; 
+ nameOfCar = " McLaren F1"; 
+ maxSpeedMph = 240; 
+ maxSpeedKph = 386; 
+ zeroToSixty = 3.2; 
+ maxHP = 627; 
+ startingPrice = 970000; 
 }
 public void fastCar9() { 
-String nameOfCar = " Zenvo ST1"; 
-int maxSpeedMph = 233; 
-int maxSpeedKph = 374; 
-double zeroToSixty = 2.9; 
-int maxHP = 1104; 
-double startingPrice = 1225000; 
+ nameOfCar = " Zenvo ST1"; 
+ maxSpeedMph = 233; 
+ maxSpeedKph = 374; 
+ zeroToSixty = 2.9; 
+ maxHP = 1104; 
+ startingPrice = 1225000; 
 }
 public void fastCar10() { 
-String nameOfCar = " Pagani Huayra"; 
-int maxSpeedMph = 230; 
-int maxSpeedKph = 370; 
-double zeroToSixty = 3; 
-int maxHP = 720; 
-double startingPrice = 1273500; 
+ nameOfCar = " Pagani Huayra"; 
+ maxSpeedMph = 230; 
+ maxSpeedKph = 370; 
+ zeroToSixty = 3; 
+ maxHP = 720; 
+ startingPrice = 1273500; 
 }
 public void fastCar11() { 
-String nameOfCar = " Gumpert Apollo"; 
-int maxSpeedMph = 225; 
-int maxSpeedKph = 362; 
-double zeroToSixty = 3; 
-int maxHP = 650; 
-double startingPrice = 450000; 
+ nameOfCar = " Gumpert Apollo"; 
+ maxSpeedMph = 225; 
+ maxSpeedKph = 362; 
+ zeroToSixty = 3; 
+ maxHP = 650; 
+ startingPrice = 450000; 
 }
 public void fastCar12() { 
-String nameOfCar = " Noble M600"; 
-int maxSpeedMph = 225; 
-int maxSpeedKph = 362; 
-double zeroToSixty = 3.7; 
-int maxHP = 650; 
-double startingPrice = 330000; 
+ nameOfCar = " Noble M600"; 
+ maxSpeedMph = 225; 
+ maxSpeedKph = 362; 
+ zeroToSixty = 3.7; 
+ maxHP = 650; 
+ startingPrice = 330000; 
 }
 
      public static void main(String[] args) {
         // TODO code application logic here
         JavaCar someCar = new JavaCar();
-        System.out.println("Family Car:");
+        System.out.println("Average Car:");
         someCar.printVariables();
-        JavaCar speedCar = someCar;
-        speedCar.fastCar1();
-        speedCar.printVariables();
-        speedCar.fastCar2();
-        speedCar.fastCar3();
-        speedCar.fastCar4();
-        speedCar.fastCar5();
-        speedCar.fastCar6();
-        speedCar.fastCar7();
-        speedCar.fastCar8();
-        speedCar.fastCar9();
-        speedCar.fastCar10();
-        speedCar.fastCar11();
-        speedCar.fastCar12();
+        System.out.println();
+                 
+        someCar.fastCar2(); 
+        someCar.printVariables(); 
+        System.out.println(); 
+        someCar.fastCar3(); 
+        someCar.printVariables(); 
+        System.out.println(); 
+        someCar.fastCar4(); 
+        someCar.printVariables(); 
+        System.out.println(); 
+        someCar.fastCar5(); 
+        someCar.printVariables(); 
+        System.out.println(); 
+        someCar.fastCar6(); 
+        someCar.printVariables(); 
+        System.out.println(); 
+        someCar.fastCar7(); 
+        someCar.printVariables(); 
+        System.out.println(); 
+        someCar.fastCar8(); 
+        someCar.printVariables(); 
+        System.out.println(); 
+        someCar.fastCar9(); 
+        someCar.printVariables(); 
+        System.out.println(); 
+        someCar.fastCar10(); 
+        someCar.printVariables(); 
+        System.out.println(); 
+        someCar.fastCar11(); 
+        someCar.printVariables(); 
+        System.out.println(); 
+        someCar.fastCar12(); 
+        someCar.printVariables(); 
+        System.out.println(); 
     }
 }
